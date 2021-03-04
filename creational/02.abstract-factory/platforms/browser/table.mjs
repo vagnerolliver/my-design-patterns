da6d74f3e842e0c5ed8637d04d05a3b752e6fcd2
Vágner Oliveira <vagner.oliveira@zenvia.com>
2021-03-04 08:07:23 -0300

    rendered table platform browser

--- creational/02.abstract-factory/platforms/browser/table.mjs
+++ creational/02.abstract-factory/platforms/browser/table.mjs
@@ -1,6 +1,37 @@
 import TableComponent from './../../shared/base/tableComponent.mjs'
 export default class TableBrowserComponent extends TableComponent {
     render(data) {
-        window.alert('Chamou!!')
+        const template  = this.prepareData(data)
+        document.body.insertAdjacentHTML('afterBegin', template)
+    }
+
+    prepareData(data) {
+        const [firstItem ] = data
+        const theaders = Object.keys(firstItem)
+                    .map(text => `<th scope='col'>${text}</th>`)
+
+        const joinLists = list => list.join('')
+
+        console.log('data', data)
+
+        const tbodyValues = data
+                    .map(item => Object.values(item))
+                    .map(item => item.map(value => `<td>${value}</td>`))
+                    .map(tds => `<tr>${joinLists(tds)}</tr>`)
+
+        const template = `
+            <table class="table">
+                <thead>
+                    <tr>
+                        ${joinLists(theaders)}
+                    </tr>
+                </thead>
+                <tbody>
+                    ${joinLists(tbodyValues)}
+                </tbody>
+            </table>
+        `
+
+        return template
     }
 }
